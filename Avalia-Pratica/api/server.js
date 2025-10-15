f9ab7ac66105ad0b56efadc461ddcc83f24da72c
larissavitoria06 <larissavitoria.ssntos12@gmail.com>
2025-10-15 16:50:22 -0300

    Atualizações recentes

--- Avalia-Pratica/api/server.js
+++ Avalia-Pratica/api/server.js
@@ -1,17 +1,13 @@
-// src/server.js
 const express = require('express');
 const cors = require('cors');
-const proferouter = require('./router/proferouter.js');
-const turmarouter = require('./routes/turmaRoutes');
-const ativirouter = require('./routes/ativirouter');
-
 const app = express();
+
+const routes = require('./src/router'); 
+
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
-app.use('/professores', proferoutes);
-app.use('/turmas', turmaroutes);
-app.use('/atividades', ativirouter);
-
-const PORT = process.env.PORT || 3006;
-app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
+app.listen(3009, () => {
+  console.log('API executando em http://localhost:3006');
+});
